117d33606bc92ea743b01cc71205373647493c55
GordonPiedallu <gordonpiedallu@gmail.com>
2026-06-24 20:32:57 +0200

    Mise à jour fichiers du site

--- produit.js
+++ produit.js
@@ -1,3 +1,31 @@
-const produitContainer = document.querySelector('.produits');
+const produitsContainer = document.getElementById('zone-produit');
 
-fetch('http://http://127.0.0.1:28147/burgers')
+fetch('produits.json', {
+    headers: {
+        'Authorization': 'Bearer '
+    }
+})
+.then(data => data.json())
+.then (data =>  {
+    console.log(data.desserts);
+            
+    data.desserts.forEach(produit => {
+        const produitElement = document.createElement('div');
+            produitElement.classList.add('produit');
+        const imgElement = document.createElement('img');
+            imgElement.classList.add('imgproduit');
+            imgElement.src = produit.image;
+            produitElement.appendChild(imgElement);
+
+        const typeElement = document.createElement('p');
+            typeElement.classList.add('type-produit');
+            typeElement.textContent = produit.nom;
+            produitElement.appendChild(typeElement);
+
+        const prixElement = document.createElement('p');
+            prixElement.classList.add('prix-produit');
+            prixElement.textContent = `${produit.prix.toFixed(2)}€`;
+            produitElement.appendChild(prixElement);
+
+            produitsContainer.appendChild(produitElement);
+    })});
