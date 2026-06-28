664e913dfcc8afa5b1c340331580bc33edc6a7a3
GordonPiedallu <gordonpiedallu@gmail.com>
2026-06-28 10:04:27 +0200

    Misajour

--- produit.js
+++ produit.js
@@ -1,9 +1,12 @@
 const produitsContainer = document.getElementById('zone-produit');
+let panier =[]
+
 
 function chargerProduits(categories) {
     fetch('produits.json')
         .then(data => data.json())
         .then(data => {
+            console.log(data)
             data[categories].forEach(produit => {
                 const produitElement = document.createElement('div');
                 produitElement.classList.add('produit');;                
@@ -18,8 +21,38 @@ function chargerProduits(categories) {
                 produitsContainer.appendChild(produitElement);
                 const prixElement = document.createElement('p');
                 prixElement.classList.add('prix-produit');
-                prixElement.textContent = produit.prix + '€';
+                prixElement.textContent = produit.prix.toFixed(2) + '€';
                 produitElement.appendChild(prixElement);
+                produitElement.addEventListener('click', () => {
+                    panier.push ({
+                        nom : produit.nom,
+                        prix : produit.prix,
+                        type : categories
+                    });
+afficherPanier()
+calculerTotal()                    
+                })
             })})};
 
-chargerProduits('menus');
+chargerProduits('Menus');
+
+                function afficherPanier () {
+                    const listePanier = document.querySelector('#liste-articles');
+                    listePanier.innerHTML = '';
+                    panier.forEach (article => {
+                    const articleElement = document.createElement('div');
+                    articleElement.textContent = article.nom + ' - ' + article.prix.toFixed(2) + '€';
+                    listePanier.appendChild(articleElement);
+             })};
+
+                function calculerTotal () {
+                    let total = 0;
+                    panier.forEach (article => {
+                    total += article.prix;
+                    });
+                    const prixTotal = document.getElementById('total-prix');
+                    prixTotal.textContent = total.toFixed(2) +'€';
+                    }
+                  
+                
+    
